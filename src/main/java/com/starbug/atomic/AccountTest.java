382395c1b5873a842173fef6a5c529f1c5d43697
startbug <51115577+startbug@users.noreply.github.com>
2021-07-24 22:59:33 +0800

    1.添加AtomicInteger案例 2.添加原子引用类型的案例

---
+++ src/main/java/com/starbug/atomic/AccountTest.java
@@ -0,0 +1,69 @@
+package com.starbug.atomic;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @Author Starbug
+ * @Date 2021/7/24 22:34
+ */
+public class AccountTest {
+
+    public static void main(String[] args) {
+        DecimalAccount.demo(new DecimalAccountCAS(new BigDecimal("10000")));
+    }
+
+}
+
+class DecimalAccountCAS implements DecimalAccount {
+
+    private AtomicReference<BigDecimal> balance;
+
+    public DecimalAccountCAS(BigDecimal balance) {
+        this.balance = new AtomicReference(balance);
+    }
+
+    @Override
+    public BigDecimal getBalance() {
+        return balance.get();
+    }
+
+    @Override
+    public void withdraw(BigDecimal amount) {
+        BigDecimal pre, next;
+        do {
+            pre = balance.get();
+            next = pre.subtract(amount);
+        } while (!balance.compareAndSet(pre, next));
+    }
+}
+
+interface DecimalAccount {
+
+    //获取余额
+    BigDecimal getBalance();
+
+    void withdraw(BigDecimal amount);
+
+    static void demo(DecimalAccount account) {
+        List<Thread> ts = new ArrayList();
+        for (int i = 0; i < 1000; i++) {
+            ts.add(new Thread(() -> {
+                account.withdraw(BigDecimal.TEN);
+            }));
+        }
+
+        ts.forEach(Thread::start);
+        ts.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        System.out.println(account.getBalance());
+    }
+}
--- src/main/java/com/starbug/atomic/TestAtomicInteger.java
+++ src/main/java/com/starbug/atomic/TestAtomicInteger.java
@@ -1,5 +1,6 @@
 package com.starbug.atomic;
 
+import java.time.temporal.ValueRange;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -10,15 +11,27 @@ public class TestAtomicInteger {
 
     public static void main(String[] args) {
 
-        AtomicInteger i = new AtomicInteger(0);
+        AtomicInteger i1 = new AtomicInteger(0);
 
-        System.out.println(i.getAndIncrement());    // i++ 自增后返回自增前的值 0
-        System.out.println(i.incrementAndGet());    // ++i 返回自增后的值      2
+        System.out.println(i1.getAndIncrement());    // i++ 自增后返回自增前的值 0
+        System.out.println(i1.incrementAndGet());    // ++i 返回自增后的值      2
 
-        System.out.println(i.getAndAdd(5)); //先获取,再+5
-        System.out.println(i.addAndGet(5)); //先+5再获取值
+        System.out.println(i1.getAndAdd(5)); //先获取,再+5
+        System.out.println(i1.addAndGet(5)); //先+5再获取值
+        System.out.println(i1.get());
 
-        System.out.println(i.get());
+        System.out.println("========================111111111=========================");
+
+        AtomicInteger i2 = new AtomicInteger(2);
+        //返回计算后的结果(设置新的值会使用CAS判断,不断自旋,直到成功)
+        int sufResult = i2.updateAndGet(value -> value * 5);
+        System.out.println(sufResult);  //10
+
+        //返回计算前的值(设置新的值会使用CAS判断,不断自旋,直到成功)
+        int preResult = i2.getAndUpdate(value -> value - 5);
+        System.out.println(preResult);
+
+        System.out.println(i2.get());
 
     }
 
